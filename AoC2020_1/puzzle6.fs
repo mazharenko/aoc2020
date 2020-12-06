cd65d5f8fea3b81a894748d39773b9b0f21717e2
Alexey Mazharenko <mazharenko.a@yandex.ru>
2020-12-06 23:27:10 +0500

    day 6

---
+++ AoC2020_1/puzzle6.fs
@@ -0,0 +1,34 @@
+#if !INTERACTIVE
+module Puzzle6
+
+#else
+#load "common.fs"
+#endif
+
+open System
+open System.IO
+
+let input =
+    let text = File.ReadAllText (Path.Combine(__SOURCE_DIRECTORY__, "puzzle6.txt"))
+    text.Split ("\n\n", StringSplitOptions.RemoveEmptyEntries)
+    
+let puzzle6 countGroup =
+    input
+    |> Seq.map (fun x -> x.Split ("\n", StringSplitOptions.RemoveEmptyEntries))
+    |> Seq.map countGroup
+    |> Seq.sum
+    
+let puzzle6_1 = 
+    let countGroup gLines =
+        gLines
+        |> Seq.collect id
+        |> Set.ofSeq
+        |> Seq.length
+    puzzle6 countGroup
+let puzzle6_2 = 
+    let countGroup gLines =
+        gLines
+        |> Seq.map Set.ofSeq
+        |> Set.intersectMany
+        |> Seq.length
+    puzzle6 countGroup
